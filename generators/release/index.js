82c0d36b55e848e8053b9cb1b310db19a3d099a4
Seth Bolander <bol15015@byui.edu>
2019-04-04 16:57:42 -0600

    began adding versioning subgenerator

---
+++ generators/release/index.js
@@ -0,0 +1,42 @@
+const ByuiConfig = require('../ByuiConfig.js');
+
+module.exports = class Release extends ByuiConfig {
+    constructor(args, opts) {
+        super(args, opts);
+    }
+
+    async initializing() {
+        await super.initializing();
+    }
+
+    async prompting() {
+        const simpleGit = require('simple-git');
+        // Find all branches of current repo
+        /* THIS IS A SINGLE FUNCTION
+        let branches;
+        await simpleGit().branch((err, branchSummary) => {
+            if (err) throw new Error(err.message);
+            branches = Object.keys(summary.branches);
+        });
+        branches = branches.filter(branch => !branch.includes('/'));
+        */
+
+        // Prompt user for branch to merge to master
+        /* THIS IS A SINGLE FUNCTION
+        var questionsToAsk = [{
+            name: 'branch',
+            type: 'input',
+            message: questionTools.messagePadEnd('What branch is being merged'),
+            suffix: ':',
+            validate: value => branches.includes(value) ? true : "That branch doesn't exist"
+        }];
+        this.prompt(questionsToAsk).then(answers => {
+            await simpleGit()
+                .push('origin', answers.branch)
+                .checkout('master', (err) => { if (err) throw new Error(err.message) })
+                .mergeFromTo(answers.branch, 'master', (err) => { if (err) throw new Error(err.message) })
+                .push('origin', 'master');
+        });
+        */
+    }
+};
